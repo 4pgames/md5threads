223249d28e642d1f43c9d6ef9428a9da088765a1
4pgames <t77uzq5dmz@liamekaens.com>
2020-06-13 01:55:14 -0400

    added MD5 function+ssl imports

--- posix-semaphore-example.c
+++ posix-semaphore-example.c
@@ -6,6 +6,7 @@
  
  // https://www.softprayog.in/programming/posix-semaphores
  // gcc posix-semaphore-example.c -o posix-semaphore-example -lpthread
+ // gcc posix-semaphore-example.c -o posix-semaphore-example -lpthread -lcrypto -lssl
 
 #include <sys/types.h>
 #include <stdio.h>
@@ -33,6 +34,54 @@ sem_t *mutex_sem, *buffer_count_sem, *spool_signal_sem;
 
 void *producer (void *arg);
 void *spooler (void *arg);
+
+// https://stackoverflow.com/questions/7627723/how-to-create-a-md5-hash-of-a-string-in-c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#if defined(__APPLE__)
+#  define COMMON_DIGEST_FOR_OPENSSL
+#  include <CommonCrypto/CommonDigest.h>
+#  define SHA1 CC_SHA1
+#else
+#  include <openssl/md5.h>
+#endif
+
+//gcc MD5.c -lcrypto -lssl
+//sudo dnf install openssl-devel
+// libssl-dev on ubuntu?
+
+// verify-1-HelloWorld
+// 3DD27BEB687E2C901480DE72728858A4
+
+
+char *str2md5(const char *str, int length) {
+    int n;
+    MD5_CTX c;
+    unsigned char digest[16];
+    char *out = (char*)malloc(33);
+
+    MD5_Init(&c);
+
+    while (length > 0) {
+        if (length > 512) {
+            MD5_Update(&c, str, 512);
+        } else {
+            MD5_Update(&c, str, length);
+        }
+        length -= 512;
+        str += 512;
+    }
+
+    MD5_Final(digest, &c);
+
+    for (n = 0; n < 16; ++n) {
+        snprintf(&(out[n*2]), 16*2, "%02x", (unsigned int)digest[n]);
+    }
+
+    return out;
+}
 
 int main (int argc, char **argv)
 {
